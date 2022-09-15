48a9d5fd0616e33525f3d7b934ae9281f2338c72
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-15 01:09:25 -0400

    Scanner hw

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        //creating scanner object
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = input.nextLine();
+        //name = input.next();
+        int age = input.nextInt();
+        // if statements
+         if (name.equals("Bob") ){
+            System.out.println("Hello welcome " + name + " you are " + age);
+        }
+         else if(name.equals("Alicia")){
+        System.out.println("Hello welcome " + name + age + " you are " + age );}
+        else{
+            System.out.println("You are not either");}
+
+}}
