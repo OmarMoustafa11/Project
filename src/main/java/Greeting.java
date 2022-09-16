86aed401f4baf5ded7c841251c7571991989a145
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-16 14:25:24 -0400

    Alice and Bob HW

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Greeting {
     public static void main(String[] args) {
         //creating scanner object
+        while(true){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter name");
         String name = input.nextLine();
-        //name = input.next();
         int age = input.nextInt();
+
+
         // if statements
          if (name.equals("Bob") ){
             System.out.println("Hello welcome " + name + " you are " + age + " years old ") ;
+            break;
         }
-         else if(name.equals("Alicia")){
-        System.out.println("Hello welcome " + name + age + " you are " + age + " years old ");}
-        else{
-            System.out.println("You are not either");}
+         else if(name.equals("Alice")) {
+             System.out.println("Hello welcome " + name + " you are " + age + " years old ");
+         break;}
 
-}}
+        else{
+            System.out.println("You do not belong here!!!!");}
+
+}}}
